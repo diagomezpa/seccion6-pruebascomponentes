035ec021bfc9f0c6dc49251f929641216247a4be
Diagomezpa <Diagomezpa@gmail.com>
2025-06-20 07:14:47 -0500

    seccion 6 video 78. Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seccion6_componentes/theme/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
    
@@ -6,9 +7,23 @@ class CardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Card Screen'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: [ListTile(
+                leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+                title: Text('Soy un título'),
+                subtitle: Text('Laborum velit reprehenderit duis consectetur labore. Cupidatat eiusmod minim ut occaecat nisi sunt ea. Incididunt culpa cillum laborum ex occaecat. Dolore exercitation consectetur amet magna in tempor cupidatat mollit excepteur ut irure reprehenderit. Excepteur voluptate est do proident incididunt eu ad dolor adipisicing sunt. Aute elit dolore ex eiusmod labore voluptate aliqua. In nostrud do laboris dolore nulla dolore duis duis esse laborum incididunt.'),
+              )],
+            ),
+          )
+        ],
       ),
     );
   }
